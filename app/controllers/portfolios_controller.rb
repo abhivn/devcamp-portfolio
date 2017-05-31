c854910a15666194c1f677466a87d63267ca1403
Rails Udemy <abhigeek8@gmail.com>
2017-06-01 00:25:19 +0530

    Integrated edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,7 @@
 class PortfoliosController < ApplicationController
 
+  before_action :set_portfolio, only: [:edit, :destroy, :update, :show]
+
   def index
     @portfolios = Portfolio.all
   end
@@ -17,6 +19,24 @@ class PortfoliosController < ApplicationController
         format.html { render :new }
       end
     end
+  end
+
+  def edit
+
+  end
+
+  def update
+    respond_to do |format|
+      if @portfolio.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def set_portfolio
+    @portfolio = Portfolio.find(params[:id])
   end
 
 
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,2 +1,6 @@
 class Portfolio < ApplicationRecord
+
+  validates :title, presence: true
+  validates :subtitle, presence: true
+  validates :body, presence: true
 end
